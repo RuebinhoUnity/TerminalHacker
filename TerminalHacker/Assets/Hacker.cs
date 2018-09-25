5913ca336447ebdabea32b614c767d65c154fa67
Dennis Rübner <dennis.ruebner@gmail.com>
2018-09-25 21:12:32 +0200

    added welcome message and variable

---
+++ TerminalHacker/Assets/Hacker.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour {
+
+    private string greeting = "Hello Hackerman!";
+
+	// Use this for initialization
+	void Start ()
+    {
+        print("Hello Console!");
+        ShowMainMenu(greeting);
+    }
+
+    private void ShowMainMenu(string greeting)
+    {
+        Terminal.ClearScreen();
+        Terminal.WriteLine(greeting);
+        Terminal.WriteLine("What do you want to hack ?");
+        Terminal.WriteLine("Press <1> for Hotel Concorde");
+        Terminal.WriteLine("Press <2> for Finanzamt");
+        Terminal.WriteLine("Press <3> for AOK Systems");
+        Terminal.WriteLine(" ");
+        Terminal.WriteLine("Enter your selection:");
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
